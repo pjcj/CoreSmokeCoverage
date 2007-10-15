476e49da821039d5c87f395adf92f50b29693c7e
abeltje <abeltje@f9e6db9c-56bf-0310-a82a-abbfb51efdf0>
2007-10-15 22:25:30 +0000

    * [CHANGE]   + Also a CPAN::Config for Linux homedir layout
    git-svn-id: https://source.test-smoke.org/svn/buildgcov@1125 f9e6db9c-56bf-0310-a82a-abbfb51efdf0

---
+++ CPAN-Config-Linux.pm
@@ -0,0 +1,57 @@
+
+# This is CPAN.pm's systemwide configuration file. This file provides
+# defaults for users, and the values can be changed in a per-user
+# configuration file. The user-config file is being looked for as
+# ~/.cpan/CPAN/MyConfig.pm.
+
+$CPAN::Config = {
+  'auto_commit' => q[0],
+  'build_cache' => q[100],
+  'build_dir' => q[/home/abeltje/Test-Smoke/perl-current-gcov/ext],
+  'bzip2' => q[/usr/bin/bzip2],
+  'cache_metadata' => q[1],
+  'commandnumber_in_prompt' => q[1],
+  'cpan_home' => q[/home/abeltje/Test-Smoke/.cpan],
+  'curl' => q[],
+  'ftp' => q[/usr/bin/ftp],
+  'ftp_passive' => q[1],
+  'ftp_proxy' => q[],
+  'getcwd' => q[cwd],
+  'gpg' => q[/usr/bin/gpg],
+  'gzip' => q[/bin/gzip],
+  'histfile' => q[/home/abeltje/Test-Smoke/.cpan/histfile],
+  'histsize' => q[100],
+  'http_proxy' => q[],
+  'inactivity_timeout' => q[0],
+  'index_expire' => q[1],
+  'inhibit_startup_message' => q[0],
+  'keep_source_where' => q[/home/abeltje/Test-Smoke/.cpan/sources],
+  'lynx' => q[],
+  'make' => q[/usr/bin/make],
+  'make_arg' => q[],
+  'make_install_arg' => q[UNINST=1],
+  'make_install_make_command' => q[/usr/bin/make],
+  'makepl_arg' => q[],
+  'mbuild_arg' => q[],
+  'mbuild_install_arg' => q[--uninst 1],
+  'mbuild_install_build_command' => q[./Build],
+  'mbuildpl_arg' => q[],
+  'ncftp' => q[],
+  'ncftpget' => q[],
+  'no_proxy' => q[],
+  'pager' => q[less],
+  'prefer_installer' => q[EUMM],
+  'prerequisites_policy' => q[follow],
+  'scan_cache' => q[atstart],
+  'shell' => q[/bin/sh],
+  'show_upload_date' => q[1],
+  'tar' => q[/bin/tar],
+  'term_is_latin' => q[1],
+  'term_ornaments' => q[1],
+  'unzip' => q[/usr/bin/unzip],
+  'urllist' => [q[ftp://ftp.cpan.nl/pub/CPAN/], q[ftp://cpan.wanadoo.nl/pub/CPAN], q[ftp://download.ip2.net/pub/mirror/CPAN/], q[ftp://download.xs4all.nl/pub/mirror/CPAN/]],
+  'use_sqlite' => q[0],
+  'wget' => q[/usr/bin/wget],
+};
+1;
+__END__
